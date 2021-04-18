41ea085370e71efc6e5d1067bcbd940930b8d139
WilsonKinyua <wilsonkinyuam@gmail.com>
2021-04-18 23:47:53 +0300

    dynamic questions

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,43 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  int questionIndex = 0;
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color',
+      'What\'s your favourite animal',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.pinkAccent,
           title: Text('Quiz App'),
         ),
-        body: Text('I am the body'),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer one'),
+            ),
+            RaisedButton(
+              onPressed: () => print('hello answer two'),
+              child: Text('Answer two'),
+            ),
+            RaisedButton(
+              child: Text('Answer Three'),
+              onPressed: () {
+                print('answer three');
+              },
+            )
+          ],
+        ),
       ),
     );
   }
